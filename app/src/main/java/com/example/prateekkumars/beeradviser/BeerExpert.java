63ef705ac8b5df6ee309681fb91b8ba684e62245
vikasmca2k11 <psinha6@gmail.com>
2017-03-20 00:24:44 +0530

    Added multiple string support

--- app/src/main/java/com/example/prateekkumars/beeradviser/BeerExpert.java
+++ app/src/main/java/com/example/prateekkumars/beeradviser/BeerExpert.java
@@ -13,7 +13,7 @@ public class BeerExpert {
         List<String> brands = new ArrayList<String>();
         if(color.equals("amber")){
             brands.add("Jack Amber");
-            brands.add("Jack Amber");
+            brands.add("Red Moose");
         } else {
             brands.add("Jail Pale Ale");
             brands.add("Gout Stout");
--- app/src/main/java/com/example/prateekkumars/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/prateekkumars/beeradviser/FindBeerActivity.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+import java.util.List;
 
+public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +17,17 @@ public class FindBeerActivity extends AppCompatActivity {
     }
 
     public void onClickFindBeer(View view){
+
         TextView brands = (TextView) findViewById(R.id.brands);
 
         Spinner color = (Spinner) findViewById(R.id.color);
 
         String beerType = String.valueOf(color.getSelectedItem());
-
-        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand: brandsList){
+            brandsFormatted.append(brand).append("\n");
+        }
+        brands.setText(brandsFormatted);
     }
 }
